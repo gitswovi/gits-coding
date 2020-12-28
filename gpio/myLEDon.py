f1be02eb93a09b1a47d6042426b67bf3036beee4
gitswovi <wovi@sh3ll.io>
2020-12-28 13:09:16 -0500

    new folder gpio

---
+++ gpio/myLEDon.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+# From https://raspberrytips.com/raspberry-pi-gpio-pins/
+
+#import libraries
+import RPi.GPIO as GPIO
+import time
+
+#GPIO Basic initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#Use a variable for the  Pin to use
+#If you followed my pictures, it's port 7 => BCM 4
+led = 4
+
+#Initialize your pin
+GPIO.setup(led,GPIO.OUT)
+
+#Turn on the LED
+print ("LED on")
+GPIO.output(led,1)
+
+#Wait 5s
+time.sleep(5)
+
+#Turn off the LED
+print ("LED off")
+GPIO.output(led,0)
